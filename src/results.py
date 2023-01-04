3c1fe64e212ae7b4b6cd85d2e1f83bcd5bf99270
Ziyyad Ali <ziyyad.ali@mail.utoronto.ca>
2023-01-03 19:22:49 -0500

    removed notes

--- src/results.py
+++ src/results.py
@@ -1,20 +1,3 @@
-"""
-class:
-    - system
-    - lnlike
-    - post
-    - version_num
-    - curr_pos
-
-    init: 
-        - set all params
-    
-    - add_samples copy-paste
-    - save_results : dont need samplername and version num but copypaste
-    - load_results: same as above
-    
-- system should be saved too (to know starting values wtc)
-"""
 import numpy as np
 
 class Results():
